684e04fbca1c75fbb353c5394a09c973063bf5e2
JammingJams <noahlogan20o00@gmail.com>
2026-04-15 10:52:09 -0400

    Initial Commit

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -1,4 +1,41 @@
 package com.pluralsight;
 
 public class CellPhone {
+
+    private int serialNumber;
+    private String model;
+    private String carrier;
+    private String phoneNumber;
+    private String userName;
+
+    public CellPhone(int serialNumber, String model, String carrier, String phoneNumber, String userName) {
+        this.serialNumber = serialNumber;
+        this.model = model;
+        this.carrier = carrier;
+        this.phoneNumber = phoneNumber;
+        this.userName = userName;
+
+    }
+
+    public int getSerialNumber() {
+        return  this.serialNumber;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public String getCarrier() {
+        return this.carrier;
+    }
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public String getUserName() {
+        return this.userName;
+    }
+
+
 }
--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,4 +1,39 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class CellPhoneApplication {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("What is your serial number? ");
+        int serialNumber = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("What is the model of your phone? ");
+        String model = sc.nextLine();
+
+        System.out.print("Who is your phone carrier? ");
+        String carrier = sc.nextLine();
+
+        System.out.print("What is your phone number? ");
+        String phoneNumber = sc.nextLine();
+
+        System.out.print("Who is the owner of this phone? ");
+        String owner = sc.nextLine();
+
+
+
+        CellPhone userPhone = new CellPhone(serialNumber,model, carrier, phoneNumber,  owner); {
+
+        }
+
+        System.out.println("Your phone's serial number is: " + userPhone.getSerialNumber());
+        System.out.println("Your phone's model is: " + userPhone.getModel());
+        System.out.println("Your phone's carrier is: " + userPhone.getCarrier());
+        System.out.println("Your phone's phone number is: " + userPhone.getSerialNumber());
+        System.out.println("Your phone's owner is: " + userPhone.getUserName());
+
+
+    }
 }
